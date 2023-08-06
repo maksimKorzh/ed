c56bc145cabbbeb80769c446b78aa97808f416c1
Code Monkey King <freesoft.for.people@gmail.com>
2023-08-06 14:54:12 +0300

    fixed buffer size on lndelete

--- src/lndelete.go
+++ src/lndelete.go
@@ -5,7 +5,7 @@ func lndelete(n1, n2 int, status *stcode) stcode {
   if n1 <= 0 {
     *status = ERR
   } else {
-    newbuf := make([]buftype, len(buf) - (n2 - n1))
+    newbuf := make([]buftype, len(buf) - (n2 - n1) - 1)
     copy(newbuf[:n1], buf[:n1])
     copy(newbuf[n1:], buf[n2+1:])
     buf = newbuf
