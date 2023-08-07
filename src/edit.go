fed0b38dc64e6e0083ea573628d6b4cfb0321b02
Code Monkey King <freesoft.for.people@gmail.com>
2023-08-07 13:29:14 +0300

    fixed empty buf on start

--- src/edit.go
+++ src/edit.go
@@ -5,8 +5,8 @@ import "fmt"
 
 /* edit -- main routine for text editor */
 func main() {
+  setbuf()
   if len(os.Args) > 1 {
-    setbuf()
     savefile = os.Args[1]
     status := doread(0, savefile)
     if status == ERR { fmt.Println("?") }
